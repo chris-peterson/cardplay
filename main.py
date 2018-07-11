787210648a7b2b1e1091cea5aaf676ce9e283097
Chris Peterson <chris.peterson@gettyimages.com>
2018-07-11 08:30:07 -0700

    Add a diag card

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import keyboard
-import yaml
+import psutil
 import soco
 import webbrowser
+import yaml
 
 library = yaml.load(open('library.yml'))
 
-sonos=None
-for zone in soco.discover():
-    if zone.player_name == 'Media Room':
-        sonos = zone
+try:
+    sonos=None
+    for zone in soco.discover():
+        if zone.player_name == 'Media Room':
+            sonos = zone
+except:
+    print('no sonos found')
+    sonos=None
 
 while True:
     print('waiting for event...')
@@ -32,6 +37,9 @@ while True:
         meta = card['meta']
         print('\t(' + action + ') - ' + title)
         if action == 'sonos':
+            if not sonos:
+                print('sonos not available, please restart application')
+                continue
             if meta == 'playlist':
                 sonos.clear_queue()
                 sonos.add_uri_to_queue(uri)
@@ -42,6 +50,14 @@ while True:
                 sonos.play_uri(uri=uri, title=title, meta=meta)
         elif action == 'browser':
             webbrowser.get(meta).open_new_tab(uri)
+        elif action == 'diag':
+            print('killing all firefox processes...')
+            for proc in psutil.process_iter():
+                if proc.status() != 'zombie' and proc.name() == 'firefox':
+                    proc.kill()
+            if sonos:
+                print('setting sonos input to line in')
+                sonos.switch_to_line_in()
         else:
             print('\t|-> unsupported')
     else:
